1b0c23b2d0fe3db828a7caef053edd17ed933e66
dinaaasen <65859588+dinaaasen@users.noreply.github.com>
2020-06-07 21:40:35 +0200

    Create level-1.js

---
+++ level-1.js
@@ -0,0 +1,28 @@
+// Question 1
+var pet = "cat";
+console.log(pet);
+
+// Question 2
+var person {
+name: "Dina",
+age: 20
+};
+
+// Question 3
+var outOfStock = "true";
+
+if(outOfStock === true) {
+  console.log ("Out Of Stock");
+}
+else {
+  console.log ("In Stock");
+  }
+  
+// Question 4
+var numbers = [1, 2, 3, 4, 5];
+
+for(var i=0; i < cars.length; i++){
+  console.log(i);
+  }
+  
+// Question 5
